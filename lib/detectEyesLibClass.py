bacfa8546b396c9dff4586f1bf3d226f227f40db
Alla Aleks <aleks.a.v@bk.ru>
2020-03-11 23:41:55 +0300

    Add irises class

--- lib/detectEyesLibClass.py
+++ lib/detectEyesLibClass.py
@@ -1,11 +1,9 @@
 # import the necessary packages
 import cv2
+import numpy as np
 import lib.detectEyesColorLib
     
-class Face():
-    # Load haarcascades
-    eyeCascade = cv2.CascadeClassifier('./haarcascades/haarcascade_eye.xml')
-    
+class Face():    
     def __init__(self, image):
         self.image = image
         
@@ -25,8 +23,50 @@ class Face():
     
     @property
     def eyes(self):
-        #face, eyes, scinColor
         eyes = detectEyes(self.image, self.gray, self.face)
+        return eyes
+    
+class Irises():
+    def __init__(self, eyesImage):
+        self.images = eyesImage
+        
+    @property
+    def eyesIrises(self):
+        irises = []
+        for eye in self.images:
+            irises.append(detectIrises(eye))
+        return irises
+            
+            
+def detectFaces(clone, gray):
+    ''' 
+    This function gets faces images,
+    detects faces to crop the image to speed up eye search.     
+    If face isn't detected, the function returns original image shape.
+    
+    Arguments:
+    
+    Returns:  
+    Faces coordinates
+    
+    '''
+       
+    # Load haarcascades
+    face_cascade = cv2.CascadeClassifier('./haarcascades/haarcascade_frontalface_default.xml')
+
+    # Find faces
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 8,
+        minSize = (20, 20)
+    )
+    
+    # Set face all image if face was not detected 
+    if len(faces)==0:
+        faces = [[0, 0, clone.shape[1], clone.shape[0]]]
+        
+    return faces
         
         
 def detectEyes(image, gray, faces):
@@ -68,34 +108,86 @@ def detectEyes(image, gray, faces):
     return eyesImages
 
 
-def detectFaces(clone, gray):
+def detectIrises(eye):
     ''' 
-    This function gets faces images,
-    detects faces to crop the image to speed up eye search.     
-    If face isn't detected, the function returns original image shape.
+    This function gets eyes images, then detects irises on processed images  
+    and returns irises which cropped by mask.  
     
     Arguments:
     
-    Returns:  
-    Faces coordinates
+    Returns:
     
-    '''
-       
-    # Load haarcascades
-    face_cascade = cv2.CascadeClassifier('./haarcascades/haarcascade_frontalface_default.xml')
+    '''       
 
-    # Find faces
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 8,
-        minSize = (20, 20)
-    )
+    clone = eye.copy()
     
-    # Set face all image if face was not detected 
-    if len(faces)==0:
-        faces = [[0, 0, clone.shape[1], clone.shape[0]]]
+    # Get processed images
+    images, imagesName = processImage(clone)
+    
+    # Find irises and write them
+    irisesImages = []
+    irisesImagesNames = []
+    for i in range(len(images)):
+        # Find iris on current image of eye
+        irises = findCirclesByMask(eye.copy(), images[i])
         
-    return faces
+        # TODO: implement finding iris in circles
+        # iris = findIris(circles)
+        
+        for j, iris in enumerate(irises):
+            if iris.size != 0:
+                # Save images for transfer
+                irisesImages.append(iris)
+                # Debug for manual checking: save images of eyes
+                #cv2.imwrite("./labeled/detectEye/" + name+ "_" + imagesName[i] + "_iris" + str(j) + ".jpg", iris)
+                #irisesImagesNames.append(name+ "_" + imagesName[i] + "_iris" + str(j))
+        
+    return irisesImages       
 
+def processImage(clone):
+    # Use bilateralFilter and convert to grayscale
+    image = cv2.bilateralFilter(clone, 10, 100, 100)
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    return [grey], ['grey']
+
+def findCirclesByMask(image, changed):
         
+    # Find circles 
+    rows  = changed.shape[0]
+    circles = cv2.HoughCircles(changed, cv2.HOUGH_GRADIENT, 1, rows/8,
+                               param1 = 100, param2 = 30,
+                               minRadius = 10, maxRadius = 100)
+        
+    crop = []    
+    # Find all circles with using mask
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            # Save original image
+            clone = image.copy()
+            
+            # Create mask
+            height,width,_ = clone.shape
+            mask = np.zeros((height, width), np.uint8)
+            center = (i[0], i[1])
+            
+            # Draw on mask
+            cv2.circle(mask, (i[0], i[1]), i[2],(255, 255, 255), thickness=-1)
+            
+            # Copy that image using that mask
+            masked_data = cv2.bitwise_and(clone, clone, mask=mask)
+
+            # Apply Threshold
+            _,thresh = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+
+            # Find contours
+            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                # Get the bounding rect
+                x, y, w, h = cv2.boundingRect(contour)
+                # Crop masked_data
+                crop.append(masked_data[y : y + h, x : x + w])
+        
+    return crop
